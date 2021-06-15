c9b3f114afd6bc79426055c4938f974fa976c6ca
josesrodriguez610 <josesrodriguez610@gmail.com>
2021-06-15 01:17:13 -0500

    (refactor) Main Logo, Board and Count

--- Program.cs
+++ Program.cs
@@ -8,28 +8,21 @@ namespace c_project
     {
       Console.Clear();
 
+
+      // Variables
       string[] el = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
       int[][] possibilities = { new int[] { 0, 1, 2 }, new int[] { 3, 4, 5 }, new int[] { 6, 7, 8 }, new int[] { 0, 3, 6 }, new int[] { 1, 4, 7 }, new int[] { 2, 5, 8 }, new int[] { 0, 4, 8 }, new int[] { 2, 4, 6 } };
-
 
       string currentPlayer;
       int moves = 0;
       string outcome = "draw";
       bool winner = false;
-
       string player1, player2;
 
 
-      Console.WriteLine(@"___________.__         ___________               ___________             ._._.");
-      Console.WriteLine(@"\__    ___/|__| ____   \__    ___/____    ____   \__    ___/___   ____   | | |");
-      Console.WriteLine(@"  |    |   |  |/ ___\    |    |  \__  \ _/ ___\    |    | /  _ \_/ __ \  | | |");
-      Console.WriteLine(@"  |    |   |  \  \___    |    |   / __ \\  \___    |    |(  <_> )  ___/   \|\|");
-      Console.WriteLine(@"  |____|   |__|\___  >   |____|  (____  /\___  >   |____| \____/ \___  >  ____");
-      Console.WriteLine(@"                   \/                 \/     \/                      \/   \/\/");
+      mainLogo();
 
-
-
-
+      // Players information
       Console.Write("Player 1: ");
       player1 = Console.ReadLine();
       Console.Write("Player 2: ");
@@ -45,22 +38,16 @@ namespace c_project
 
         if (currentPlayer == player1)
         {
-          Console.WriteLine();
-          Console.WriteLine($" Player's turn: {currentPlayer}");
-          Console.WriteLine($" Symbol: {currentIcon}");
-          Console.WriteLine();
-          Console.WriteLine($"  {el[0]} | {el[1]} | {el[2]}");
-          Console.WriteLine($" ----------");
-          Console.WriteLine($"  {el[3]} | {el[4]} | {el[5]}");
-          Console.WriteLine($" ----------");
-          Console.WriteLine($"  {el[6]} | {el[7]} | {el[8]}");
+
+          // Tic Tac Toe Board  
+          board(currentPlayer, currentIcon, el);
 
           Console.WriteLine();
           Console.WriteLine("Where to? ");
           string temp = Console.ReadLine();
           el[Convert.ToInt32(temp) - 1] = currentIcon;
 
-
+          moves++;
           for (int j = 0; j < possibilities.Length; j++)
           {
             int[] possibilityEachArray = possibilities[j];
@@ -69,14 +56,14 @@ namespace c_project
               outcome = "winner";
               winner = true;
             }
-            else if (moves == 8)
+            else if (moves == 9)
             {
               outcome = "draw";
               winner = true;
             }
           }
 
-          moves++;
+
           Console.Clear();
           if (!winner)
           {
@@ -86,21 +73,17 @@ namespace c_project
         }
         else if (currentPlayer == player2)
         {
-          Console.WriteLine();
-          Console.WriteLine($" Player's turn: {currentPlayer}");
-          Console.WriteLine($" Symbol: {currentIcon}");
-          Console.WriteLine();
-          Console.WriteLine($"  {el[0]} | {el[1]} | {el[2]}");
-          Console.WriteLine($" ----------");
-          Console.WriteLine($"  {el[3]} | {el[4]} | {el[5]}");
-          Console.WriteLine($" ----------");
-          Console.WriteLine($"  {el[6]} | {el[7]} | {el[8]}");
+
+          // Tic Tac Toe Board  
+          board(currentPlayer, currentIcon, el);
 
           Console.WriteLine();
           Console.WriteLine(" Where to? ");
           string tempNumberValueString = Console.ReadLine();
           int tempNumberValueToIntIndex = Convert.ToInt32(tempNumberValueString) - 1;
           el[tempNumberValueToIntIndex] = currentIcon;
+
+          moves++;
 
           for (int j = 0; j < possibilities.Length; j++)
 
@@ -111,19 +94,19 @@ namespace c_project
               outcome = "winner";
               winner = true;
             }
+            else if (moves == 9)
+            {
+              outcome = "draw";
+              winner = true;
+            }
           }
 
-          moves++;
+
           Console.Clear();
 
           if (!winner)
           {
             currentPlayer = player1;
-          }
-          else if (moves == 8)
-          {
-            outcome = "draw";
-            winner = true;
           }
         }
 
@@ -133,6 +116,7 @@ namespace c_project
       {
 
         Console.WriteLine($" {currentPlayer} is the {outcome}");
+        Congrats();
       }
       else
       {
@@ -142,5 +126,47 @@ namespace c_project
 
       Console.ReadKey();
     }
+
+
+    static public void mainLogo()
+    {
+      Console.WriteLine(@"___________.__         ___________               ___________             ._._.");
+      Console.WriteLine(@"\__    ___/|__| ____   \__    ___/____    ____   \__    ___/___   ____   | | |");
+      Console.WriteLine(@"  |    |   |  |/ ___\    |    |  \__  \ _/ ___\    |    | /  _ \_/ __ \  | | |");
+      Console.WriteLine(@"  |    |   |  \  \___    |    |   / __ \\  \___    |    |(  <_> )  ___/   \|\|");
+      Console.WriteLine(@"  |____|   |__|\___  >   |____|  (____  /\___  >   |____| \____/ \___  >  ____");
+      Console.WriteLine(@"                   \/                 \/     \/                      \/   \/\/");
+
+    }
+
+
+    static public void Congrats()
+    {
+      Console.WriteLine(@" ________  ________  ________   ________  ________  ________  _________  ___  ___  ___       ________  _________  ___  ________  ________   ________");
+      Console.WriteLine(@"|\   ____\|\   __  \|\   ___  \|\   ____\|\   __  \|\   __  \|\___   ___\\  \|\  \|\  \     |\   __  \|\___   ___\\  \|\   __  \|\   ___  \|\   ____\");
+      Console.WriteLine(@"\ \  \___|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \ \  \|\  \|___ \  \_\ \  \\\  \ \  \    \ \  \|\  \|___ \  \_\ \  \ \  \|\  \ \  \\ \  \ \  \___|_");
+      Console.WriteLine(@" \ \  \    \ \  \\\  \ \  \\ \  \ \  \  __\ \   _  _\ \   __  \   \ \  \ \ \  \\\  \ \  \    \ \   __  \   \ \  \ \ \  \ \  \\\  \ \  \\ \  \ \_____  \");
+      Console.WriteLine(@"  \ \  \____\ \  \\\  \ \  \\ \  \ \  \|\  \ \  \\  \\ \  \ \  \   \ \  \ \ \  \\\  \ \  \____\ \  \ \  \   \ \  \ \ \  \ \  \\\  \ \  \\ \  \|____|\  \");
+      Console.WriteLine(@"   \ \_______\ \_______\ \__\\ \__\ \_______\ \__\\ _\\ \__\ \__\   \ \__\ \ \_______\ \_______\ \__\ \__\   \ \__\ \ \__\ \_______\ \__\\ \__\____\_\  \");
+      Console.WriteLine(@"    \|_______|\|_______|\|__| \|__|\|_______|\|__|\|__|\|__|\|__|    \|__|  \|_______|\|_______|\|__|\|__|    \|__|  \|__|\|_______|\|__| \|__|\_________\");
+      Console.WriteLine(@"                                                                                                                                              \|_________|");
+
+
+    }
+
+    static public void board(string currentPlayer, string currentIcon, string[] el)
+    {
+      Console.WriteLine();
+      Console.WriteLine($" Player's turn: {currentPlayer}");
+      Console.WriteLine($" Symbol: {currentIcon}");
+      Console.WriteLine();
+      Console.WriteLine($"  {el[0]} | {el[1]} | {el[2]}");
+      Console.WriteLine($" ----------");
+      Console.WriteLine($"  {el[3]} | {el[4]} | {el[5]}");
+      Console.WriteLine($" ----------");
+      Console.WriteLine($"  {el[6]} | {el[7]} | {el[8]}");
+    }
   }
+
+
 }
